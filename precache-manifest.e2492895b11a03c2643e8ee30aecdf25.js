af3773c16825de610b6b4ccfd71645f41b465572
Andrew <asnow4u@gmail.com>
2021-07-21 18:04:26 -0600

    Updates

---
+++ precache-manifest.e2492895b11a03c2643e8ee30aecdf25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7a54335906ff4b547bf36252a011e9",
+    "url": "/SnowPortfolio/index.html"
+  },
+  {
+    "revision": "1a2f57e5c4d5eda70c4e",
+    "url": "/SnowPortfolio/static/css/main.68f53255.chunk.css"
+  },
+  {
+    "revision": "64b3a7d5b9a688ddc4b5",
+    "url": "/SnowPortfolio/static/js/2.56acbe5b.chunk.js"
+  },
+  {
+    "revision": "63ac404d1d3777a10c65b5bf2de6bc03",
+    "url": "/SnowPortfolio/static/js/2.56acbe5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2f57e5c4d5eda70c4e",
+    "url": "/SnowPortfolio/static/js/main.b4643401.chunk.js"
+  },
+  {
+    "revision": "11a1fee2af64390018eb",
+    "url": "/SnowPortfolio/static/js/runtime-main.b6074936.js"
+  }
+]);
